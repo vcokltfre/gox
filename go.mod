5b8e049ae6826e809340a6b9da9e7d4ab1688cc9
vcokltfre <vcokltfre@gmail.com>
2023-07-19 16:39:01 +0100

    feat: add validation

--- go.mod
+++ go.mod
@@ -11,6 +11,10 @@ require (
 )
 
 require (
+	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.14.1 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
@@ -18,6 +22,7 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/mattn/go-sqlite3 v1.14.17 // indirect
---
+++ validate.go
@@ -0,0 +1,49 @@
+package gox
+
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v4"
+)
+
+var v = validator.New()
+
+type validationErrorField struct {
+	Field      string `json:"field"`
+	Tag        string `json:"tag"`
+	Constraint string `json:"constraint"`
+}
+
+type validationError struct {
+	Errors []validationErrorField `json:"errors"`
+}
+
+func (g *Gox) Validate(to any, c echo.Context) bool {
+	if err := c.Bind(to); err != nil {
+		c.JSON(400, validationError{
+			Errors: []validationErrorField{},
+		})
+
+		return false
+	}
+
+	err := v.Struct(to)
+	if err != nil {
+		fieldErrors := []validationErrorField{}
+
+		for _, err := range err.(validator.ValidationErrors) {
+			fieldErrors = append(fieldErrors, validationErrorField{
+				Field:      err.Field(),
+				Tag:        err.Tag(),
+				Constraint: err.Param(),
+			})
+		}
+
+		c.JSON(400, validationError{
+			Errors: fieldErrors,
+		})
+
+		return false
+	}
+
+	return true
+}
